1d670990687fb42c3cac603a107b55d5a567dc2c
Idris Shittu <115847693+idrisshittu02@users.noreply.github.com>
2025-09-21 23:56:49 +0100

    Develop a simple API endpoint using Django REST Framework

---
+++ api_project/api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .serializers import BookSerializer
+from rest_framework import generics, viewsets
+from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        return Book.objects.all()
+    
+class BookViewSet(viewsets.ModelViewSet): 
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        return Book.objects.all()
