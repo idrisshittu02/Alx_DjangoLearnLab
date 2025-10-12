7e18bf3a870025ec85e275096bd5d7bbd6fb21a1
Idris Shittu <115847693+idrisshittu02@users.noreply.github.com>
2025-10-12 21:21:23 +0100

    Project Setup and User Authentication

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,14 @@
-# posts/urls.py
 from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet, FeedView, LikePostView, UnlikePostView
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='post')
+router.register('comments', CommentViewSet, basename='comment')
+
 
 urlpatterns = [
-    path('feed/', views.feed, name='feed'),
-]
-
+    path('feed/', FeedView.as_view(), name='feed'),
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+] + router.urls
